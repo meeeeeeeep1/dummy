ebff32fdde336307080d0abe408550103ecfd9c1
meeeeeeeep1 <75493196+meeeeeeeep1@users.noreply.github.com>
2023-06-22 16:10:16 -0400

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,3 +9,17 @@
  * 
  * To register a constrol simmessages, use addSimMessageHandler
  */
+// Create a new button element
+var button = document.createElement("button");
+button.innerHTML = "Click Me";
+
+// Get the first element with the class name "ui items simPanel"
+var elements = document.getElementsByClassName("ui items simPanel");
+if (elements.length > 0) {
+  var targetElement = elements[0];
+
+  // Append the button to the target element
+  targetElement.appendChild(button);
+} else {
+  console.log("No elements found with the specified class name.");
+}
